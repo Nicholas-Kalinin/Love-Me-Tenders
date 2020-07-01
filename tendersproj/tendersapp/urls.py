0c55a8f22a76092535c32b5a63b2b6f09d93f159
Nicholas Kalinin <nicholasekalinin@gmail.com>
2020-07-01 15:29:57 -0400

    pre lunch push

--- tendersproj/tendersapp/urls.py
+++ tendersproj/tendersapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from. import views
+from . import views
 
 app_name = 'tendersapp'
 urlpatterns = [
-    path('index/', views.index, name='index')
+    path('index/', views.index, name='index'),
+    path('new_user/', views.new_user, name='new_user'),
 ]
--- tendersproj/tendersapp/views.py
+++ tendersproj/tendersapp/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, redirect
-from .models import TenderReview, Restaurant
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+from .models import TenderReview, Restaurant, User
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.contrib.auth.models import User
+
 
 def index(request):
     tenders_review = TenderReview.objects.all()
@@ -8,3 +12,9 @@ def index(request):
     }
     return render(request, 'tendersapp/index.html', context)
 
+def new_user(request): 
+    new_user = User()
+    context = {
+        'new_user': new_user,
+    }
+    return render(request, 'tendersapp/new_user.html', context)
